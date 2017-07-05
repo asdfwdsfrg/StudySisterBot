6129fd457f57f5704eb8c11cec22b1e07be32d9b
sssste <251612113@qq.com>
2017-07-05 20:39:43 +0800

    添加了json解析

---
+++ StudySisterBot/Controllers/ApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace StudySisterBot.Controllers
+{
+    public class ApiController
+    {
+        public class ResultData
+        {
+            public string type;
+            public List<string> result;
+
+        }
+       public string GetObject(string data)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string url = "http://bop.pescn.cc/";
+                var json = wc.DownloadString(url+data);
+                ResultData rs = (ResultData)JsonConvert.DeserializeObject(json);
+
+                return json;
+            }
+        }
+
+    }
+}
