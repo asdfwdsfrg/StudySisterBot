e2954957e8950884049f0d97d0d3575a0b5cb8eb
sssste <251612113@qq.com>
2017-07-06 00:41:29 +0800

    添加了属性查询

--- StudySisterBot/Dialogs/RootLuisDialog.cs
+++ StudySisterBot/Dialogs/RootLuisDialog.cs
@@ -4,6 +4,8 @@ using Microsoft.Bot.Builder.Dialogs;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Collections.Generic;
+using StudySisterBot.Controllers;
 
 namespace StudySisterBot.Dialogs
 {
@@ -11,6 +13,18 @@ namespace StudySisterBot.Dialogs
     [Serializable]
     public class RootLuisDialog : LuisDialog<object>
     {
+        public bool iserror(DBController.ResultData e)
+        {
+            return e.type == "error";
+        }
+
+        public string getAllValue(DBController.ResultData d)
+        {
+            string s= "";
+            foreach (var each in d.result) s = s + each + ' ';
+            return s;
+        }
+
 
         [LuisIntent("")]
         [LuisIntent("None")]
@@ -31,9 +45,59 @@ namespace StudySisterBot.Dialogs
         [LuisIntent("QueryAttribution")]
         public async Task QueryAttribution(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
         {
-            result
-            var message = await activity;
-            await context.PostAsync("You QueryAttribution");        
+            if (result.Entities.Count < 1)
+            {
+                var message = await activity;
+                await context.PostAsync("呀呀呀没找到呀！");
+            }
+            else if(result.Entities.Count == 1)
+            {
+                EntityRecommendation e1 = null;
+                foreach (var each in result.Entities)
+                {
+                    e1 = each;
+                }
+                var obj = DBController.GetObject("relations?entity=西安电子科技大学&relation=" + e1.ToString());
+                if (!iserror(obj))
+                {
+                    string s = getAllValue(obj);
+                    var message = await activity;
+                    await context.PostAsync(s);
+                }
+                else
+                {
+                    var message = await activity;
+                    await context.PostAsync("抱歉没找到哦~");
+                }
+            }
+            else
+            {
+                EntityRecommendation e1 = null, e2 = null;
+                foreach (var each in result.Entities)
+                {
+                    if (each.Type== "学院" || each.Type =="部门")
+                    {
+                        e1 = each;
+                    }
+                    else
+                    {
+                        e2 = each;
+                    }
+                }
+                var obj = DBController.GetObject("relations?entity="+e1.ToString()+"&relation="+e2.ToString());
+                if (!iserror(obj))
+                {
+                    string s = getAllValue(obj);
+                    var message = await activity;
+                    await context.PostAsync(s);
+                }else
+                {
+                    var message = await activity;
+                    await context.PostAsync("抱歉没找到哦~");
+                }
+
+            }
+  
         }
     }
 }
